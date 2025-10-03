1b5dc7209ac19cd1ded92efce89dde364cdfe0cc
haonguyen <nguyenminhhao1188@gmail.com>
2025-10-03 21:37:55 +0700

    llm change

--- app/llm_model/routes.py
+++ app/llm_model/routes.py
@@ -7,10 +7,7 @@ from uuid import UUID
 
 chat_router = APIRouter()
 
-
 @chat_router.post("/{chat_id}")
 async def chat_endpoint(chat_id: str, question: str, session: SessionDep):
-    event_stream = await generate_response(
-        query=question, chat_id=chat_id, session=session
-    )
+    event_stream = await generate_response(query=question, chat_id=chat_id, session=session)
     return StreamingResponse(event_stream(), media_type="text/plain")
